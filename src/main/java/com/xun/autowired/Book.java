8bd7022e1fe98e6ad672808a1d515d63614bc590
zhangzhe <zhangzhe_1518@163.com>
2018-12-11 22:20:51 +0800

    bean的自动装配

---
+++ src/main/java/com/xun/autowired/Book.java
@@ -0,0 +1,32 @@
+package com.xun.autowired;
+
+public class Book {
+
+    private String bookName;
+
+    private int pageSize;
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "bookName='" + bookName + '\'' +
+                ", pageSize=" + pageSize +
+                '}';
+    }
+}
---
+++ src/main/java/com/xun/autowired/Main.java
@@ -0,0 +1,19 @@
+package com.xun.autowired;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans-autowrite.xml");
+        Teacher teacher = (Teacher) context.getBean("teacher");
+        System.out.println(teacher);
+        System.out.println("=============================");
+        Teacher teacher1 = (Teacher) context.getBean("teacher1");
+        System.out.println(teacher1);
+        System.out.println("=============================");
+        Teacher teacher2 = (Teacher) context.getBean("teacher2");
+        System.out.println(teacher2);
+    }
+}
