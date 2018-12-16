d62a44ec06a813eec4746c695b5186fff76641b3
zhangzhe <zhangzhe_1518@163.com>
2018-12-16 22:28:04 +0800

    实例工厂方法

--- src/main/java/com/xun/factory/Main.java
+++ src/main/java/com/xun/factory/Main.java
@@ -7,7 +7,10 @@ public class Main {
 
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("beans-factory.xml");
-        Animal animal = (Animal) context.getBean("animal");
+        Animal animal = (Animal) context.getBean("staticAnimalFactory");
         System.out.println(animal);
+        System.out.println("=======================");
+        Animal animal1 = (Animal) context.getBean("animal");
+        System.out.println(animal1);
     }
 }
